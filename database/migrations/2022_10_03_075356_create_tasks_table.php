74b9034145cdb5f945df99c6316bac98c6027581
giabyte_tech <giabyte_tech@ukr.net>
2022-10-03 11:34:26 +0300

    created database migrations with models

--- database/migrations/2022_10_03_075356_create_tasks_table.php
+++ database/migrations/2022_10_03_075356_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('title');
+            $table->text('description');
+            $table->bigInteger('category_id');
+            $table->timestamp('end_date');
+            $table->boolean('importance');
+            $table->bigInteger('user_id');
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 }
