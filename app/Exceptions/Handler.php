bd508e27875f0726fc36fd04f3ef60f0ceb05581
giabyte_tech <giabyte_tech@ukr.net>
2022-10-05 10:32:38 +0300

    enabled errors in json format

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,4 +38,10 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    protected function shouldReturnJson($request, Throwable $e)
+    {
+        return true;
+//        return parent::shouldReturnJson($request, $e); //
+    }
 }
