b4169860b647234a5f8bcf6fefda0ffcdc69ddde
giabyte_tech <giabyte_tech@ukr.net>
2022-10-06 19:17:07 +0300

    refactoring

--- app/Actions/StoreTaskAttachmentsAction.php
+++ app/Actions/StoreTaskAttachmentsAction.php
@@ -6,20 +6,21 @@ namespace App\Actions;
 
 use App\Http\Requests\AttachmentsRequest;
 use App\Models\Attachment;
+use Illuminate\Support\Arr;
 
 class StoreTaskAttachmentsAction
 {
-    protected $request;
+    protected $files;
 
     public function __construct(AttachmentsRequest $request)
     {
-        $this->request = $request;
+        $this->files = Arr::pluck($request->file('attachments'), 'file');
     }
 
     public function store($task_id)
     {
-        foreach ($this->request->file('attachments') as $file) {
-            $attachment = $file['file'];
+        foreach ($this->files as $file) {
+            $attachment = $file;
             $file = $attachment->store('/uploads', 'attachments');
             Attachment::create([
                 'file' => $file,
